7f66b2db8f1ff5bc9c41572737b997bbc27f1a8a
Jason Arnold <jmarn94@gmail.com>
2019-07-17 20:59:02 -0500

    updated code, no heruko app yet

--- server.js
+++ server.js
@@ -1,28 +1,95 @@
 var express = require("express");
-var handlebars = require("express-handlebars");
+// var logger = require("morgan");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require("axios");
-var bodyParser = require("body-parser");
 
 var db = require("./modals");
 
-var PORT = process.env.PORT || 3000
 var app = express();
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+app.use(logger("dev"));
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
-
-app.engine("handlebars", handlebars({ defaultLayout: "main"}));
-app.set("view engine", "handlebars");
-
-app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/router")(app,axios,cheerio,db);
 
-app.listen(PORT, function() {
-    console.log("Server connected to PORT: " + PORT);
+var DB_URI = process.env.MONGODB_URI || "mongodb://localhost/News-Scraper";
+
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true
+});
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+app.get("/json", function(req, res) {
+  db.Article.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function(req, res) {
+  axios
+    .get("https://www.statesman.com/")
+    .then(function(response) {
+      var $ = cheerio.load(response.data);
+      $("h1").each(function(i, element) {
+        var result = {};
+
+        result.title = $(this).text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
+      res.send("Scrape Complete");
+    });
+});
+
+
+
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+  
+ 
+  app.post("/articles/:id", function(req, res) {
+  
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+
 });
